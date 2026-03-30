568e9f81dc3f98bd9c9313495400586fb2dec0bf
yahya09206 <yahyahussein91@gmail.com>
2026-03-29 22:33:25 -0700

    check if list is empty or not

---
+++ Neetcode/Lists/list_len.py
@@ -0,0 +1,31 @@
+'''
+In the code editor, implement the following two functions:
+
+check_list_empty(my_list) -> bool should return a boolean representing whether the list is empty or not.
+check_element_in_list(my_list, element) -> bool should return a boolean representing whether the element is present in the list or not. 
+The list may contain integers or strings.
+'''
+
+def check_list_empty(my_list) -> bool:
+    if len(my_list) > 0:
+        return False
+    else:
+        return True
+
+
+def check_element_in_list(my_list, element) -> bool:
+    if element in my_list:
+        return True
+    else:
+        return False
+
+
+# do not modify below this line
+print(check_list_empty([]))
+print(check_list_empty([1, 2, 3]))
+
+print(check_element_in_list([1, 2, 3], 1))
+print(check_element_in_list([1, 2, 3], 4))
+
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Banana"))
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Grape"))
