f48a47221a494a881dda7c9c414c1a97426131c1
yahya09206 <yahyahussein91@gmail.com>
2026-03-29 22:51:15 -0700

    append to end of list

---
+++ Neetcode/Lists/list_append.py
@@ -0,0 +1,18 @@
+'''
+Implement a function called append_to_list(my_list: List[int], elements: List[int]) -> List[int]. 
+It should append each number from elements to the end of my_list and return the modified list.
+'''
+
+from typing import List # this is used to add type hints for List type
+
+def append_to_list(my_list: List[int], elements: List[int]) -> List[int]:
+    
+    for i in elements:
+        my_list.append(i)
+    return my_list
+
+
+
+# do not modify below this line
+print(append_to_list([1, 2, 3], [4, 5]))
+print(append_to_list([], [1, 2, 3, 4]))
