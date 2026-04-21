7b251d89f4da7e367ed4e7ae8f0d18de9c8921e1
yahya09206 <yahyahussein91@gmail.com>
2026-04-20 23:30:55 -0700

    completed function for checking which side of string

--- Leetcode/valid_palindrome_two.py
+++ Leetcode/valid_palindrome_two.py
@@ -11,7 +11,26 @@ def is_palindrome(s,l,r):
 
 	return True
 
-def valid_palindrome_two():
+def valid_palindrome_two(s):
 
-	
+	l = 0
+	r = len(s) - 1
 
+	while l < r:
+
+		if s[l] == s[r]:
+			l += 1
+			r -= 1
+
+		else:
+			return is_palindrome(s, l + 1, r) or is_palindrome(s, l, r - 1)
+
+	return True
+
+
+
+word = "aba"
+word2 = "abc"
+
+print(valid_palindrome_two(word))
+print(valid_palindrome_two(word2))
