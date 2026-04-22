600f39e668166a14366f9e44d25a13de34dccf82
yahya09206 <yahyahussein91@gmail.com>
2026-04-21 23:27:28 -0700

    revised valid palindrome 1

---
+++ Leetcode/Revised_3/valid_palindrome.py
@@ -0,0 +1,30 @@
+def valid_palindrome(s:str) -> bool:
+
+	l = 0
+	r = len(s) - 1
+
+	while l < r:
+
+		while l < r and not s[l].isalnum():
+			l += 1
+
+		while l < r and not s[r].isalnum():
+			r -= 1
+
+		if s[l].lower() != s[r].lower():
+			return False
+
+		l += 1
+		r -= 1
+
+	return True
+
+
+s1 = "racecar"
+s2 = "A man, a plan, a canal: Panama"
+s3 = "race a car"
+s4 = ".,"
+print(valid_palindrome(s1))
+print(valid_palindrome(s2))
+print(valid_palindrome(s3))
+print(valid_palindrome(s4))
