f2f51e2c7f7407f2e82bb0ec828fdab86319bcea
yahya09206 <yahyahussein91@gmail.com>
2026-05-05 22:54:48 -0700

    done with revision day 7 for reverse string ii

---
+++ Leetcode/Revised_14/valid_palindrome_ii.py
@@ -0,0 +1,28 @@
+def is_palindrome(s, l, r):
+
+	while l < r:
+
+		if s[l] != s[r]:
+			return False
+
+		l += 1
+		r -= 1
+
+	return True
+
+def valid_palindrome(s):
+
+	l = 0
+	r = len(s) - 1
+
+	while l < r:
+
+		if s[l] == s[r]:
+
+			l += 1
+			r -= 1
+		else:
+			return is_palindrome(s, l + 1, r) or is_palindrome(s, l, r - 1)
+
+	return True
+
