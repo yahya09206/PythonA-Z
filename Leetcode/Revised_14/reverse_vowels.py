ad591f9ea43d001020126271330a7cc5e3e34c0f
yahya09206 <yahyahussein91@gmail.com>
2026-05-06 23:34:17 -0700

    fixed typo

---
+++ Leetcode/Revised_14/reverse_vowels.py
@@ -0,0 +1,22 @@
+def reverse_vowels(s:str) -> str:
+
+	chars = list(s)
+	vowels = set("aeiouAEIOU")
+	l = 0
+	r = len(s) - 1
+
+	while l < r:
+
+		while l < r and chars[l] not in vowels:
+			l += 1
+
+		while l < r and chars[r] not in vowels:
+			l -= 1
+
+		if l < r:
+			chars[l], chars[r] = chars[r], chars[l]
+
+		l += 1
+		r -= 1
+
+	return "".join(chars)
