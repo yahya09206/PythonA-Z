8b5431682d1663c3da1df7d94c59ccc370a2b958
yahya09206 <yahyahussein91@gmail.com>
2026-05-07 22:43:36 -0700

    finally got the logic down

--- Leetcode/Revised_7/reverse_string_iii.py
+++ Leetcode/Revised_7/reverse_string_iii.py
@@ -6,3 +6,25 @@ def reverse_range(s, l, r):
 
 		l += 1
 		r -= 1
+
+
+def reverse_string_three(s:str) -> str:
+
+	chars = list(s)
+	start = 0
+
+	for end in range(0, len(chars)):
+
+		if chars[end] == " " or end == len(chars) - 1:
+
+			if end == len(chars) - 1:
+				r = end
+			else:
+				r = end - 1
+
+			reverse_range(chars, start, r)
+
+			start = end + 1
+
+	return "".join(chars)
+
