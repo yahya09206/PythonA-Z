2215e24c4dda4e5727b3479c341780a65916ac95
yahya09206 <yahyahussein91@gmail.com>
2026-05-09 00:29:29 -0700

    saved progress

--- Leetcode/two_sum_ii.py
+++ Leetcode/two_sum_ii.py
@@ -1,8 +1,15 @@
 def two_sum_ii(nums:List[int], target:int) -> int:
 
 	l = 0
-	r = len(nums)
+	r = len(nums) - 1
 
 	while l < r:
+		current_sum = nums[l] + nums[r]
 
-		if
+		if current_sum == target:
+			return nums[l + 1], nums[r + 1]
+		elif current_sum < target:
+			l += 1
+		else:
+			r -= 1
+
