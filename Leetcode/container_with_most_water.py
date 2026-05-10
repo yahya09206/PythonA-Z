2a3b83f9a3afe7a3997066e4eeb539cb84eaeb0a
yahya09206 <yahyahussein91@gmail.com>
2026-05-09 22:41:14 -0700

    sublime auto save is off

--- Leetcode/container_with_most_water.py
+++ Leetcode/container_with_most_water.py
@@ -1,3 +1,17 @@
 def container_with_most_water(height:List[int]) -> int:
 
-	
+	l = 0
+	r = len(height) - 1
+	ans = 0
+
+	while l < r:
+
+		area = min(height[l], height[r]) * (r - l)
+		ans = max(ans, area)
+
+		if height[l] < height[r]:
+			l += 1
+		else:
+			r -= 1
+
+	return ans
