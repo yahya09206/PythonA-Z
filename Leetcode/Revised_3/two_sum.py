52a10aa7e140a54485d0e6ce7261fa7359cd4d25
yahya09206 <yahyahussein91@gmail.com>
2026-05-16 22:08:38 -0700

    revision for day 3 two sum complete

---
+++ Leetcode/Revised_3/two_sum.py
@@ -0,0 +1,18 @@
+def two_sum(nums:List[int], target: int) -> list[int]:
+
+	seen = {}
+
+	for i, num in enumerate(nums):
+
+		complement = target - num 
+
+		if complement in seen:
+			return [seen[complement], i]
+
+		seen[num] = i 
+
+
+
+nums = [2,7,11,15]
+print(two_sum(nums, 9))
+
