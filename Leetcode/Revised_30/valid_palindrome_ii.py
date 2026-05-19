a0a02e0bde651405a14501fed089cc437b484b02
yahya09206 <yahyahussein91@gmail.com>
2026-05-18 23:02:37 -0700

    day 30 revision for valid palindrome ii

---
+++ Leetcode/Revised_30/valid_palindrome_ii.py
@@ -0,0 +1,34 @@
+def valid_palindrome(s, l, r):
+
+	while l < r:
+
+		if s[l] != s[r]:
+			return False
+
+		l += 1
+		r -= 1
+
+	return True
+
+
+def valid_palindrome_ii(s):
+
+	l = 0
+	r = len(s) - 1
+
+	while l < r:
+
+		if s[l] == s[r]:
+			l += 1
+			r -= 1
+		else:
+			return valid_palindrome(s, l + 1, r ) or valid_palindrome(s, l, r - 1)
+
+	return True
+
+
+word = "aba"
+word2 = "abc"
+
+print(valid_palindrome_ii(word))
+print(valid_palindrome_ii(word2))
