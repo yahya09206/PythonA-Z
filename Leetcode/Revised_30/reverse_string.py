0d9f2eb8f8770b620c26820a93044a58fef98528
yahya09206 <yahyahussein91@gmail.com>
2026-05-18 22:46:39 -0700

    day 30 revision for reverse string

---
+++ Leetcode/Revised_30/reverse_string.py
@@ -0,0 +1,14 @@
+def reverse_string(s:List[str]) -> None: 
+
+	l = 0
+	r = len(s) - 1
+
+	while l < r:
+
+		s[l], s[r] = s[r], s[l]
+
+		l += 1
+		r -= 1
+
+
+print(reverse_string(["h","e","l","l","o"]))
