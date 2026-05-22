b834e824d726117eff3bfda96954c3a7ec6d0379
yahya09206 <yahyahussein91@gmail.com>
2026-05-21 23:56:53 -0700

    logic for first unique character complete

--- Leetcode/first_unique_character.py
+++ Leetcode/first_unique_character.py
@@ -1,3 +1,14 @@
 def first_unique(s:str) -> int:
 
-	seen = {}
+	count_map = {}
+
+	for char in s:
+		
+		count_map[char] = count_map.get(char, 0) + 1
+
+	for i, char in enumerate(s):
+
+		if count_map[char] == 1:
+			return i
+
+	return -1
