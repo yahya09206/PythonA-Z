d1107f8a8daa10c1c8aeb972455b3400f6a29e9a
yahya09206 <yahyahussein91@gmail.com>
2026-05-22 23:18:51 -0700

    revision for day 1 logic for first unique char complete

---
+++ Leetcode/Revised_1/first_unique_char.py
@@ -0,0 +1,19 @@
+def first_unique(s:str) -> int:
+
+	count_map = {}
+
+	for char in s:
+		count_map[char] = count_map.get(char, 0) + 1
+
+	for i, char in enumerate(s):
+		if count_map[char] == 1:
+			return i 
+
+
+	return -1
+
+
+word = "leetcode"
+word2 = "loveleetcode"
+print(first_unique(word))
+print(first_unique(word2))
