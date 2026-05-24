30e92e54e9d648bdfc0f1e24ac631ae67c8b24b2
yahya09206 <yahyahussein91@gmail.com>
2026-05-24 00:21:46 -0700

    logic for group anagrams is complete

--- Leetcode/group_anagrams.py
+++ Leetcode/group_anagrams.py
@@ -1,3 +1,17 @@
 def group_anagrams(strs:List[str]) -> List[List[str]]: 
 
-	
+	groups = {}
+
+	for word in strs:
+
+		sorted_word = ''.join(sorted(word))
+
+		if sorted_word not in groups:
+
+			groups[sorted_word] = []
+
+
+		groups[sorted_word].append(word)
+
+
+	return list(groups.values())
