9d9c808abf398fd2c44eab45819bb4c883a55008
yahya09206 <yahyahussein91@gmail.com>
2026-05-24 21:56:03 -0700

    iffy on revision day 14 for contains dupe

---
+++ Leetcode/Revised_14/contains_dupe.py
@@ -0,0 +1,16 @@
+def contains(nums:List[int]) -> bool:
+
+	seen = set()
+
+	for num in nums:
+
+		if num in seen:
+			return True
+		else:
+			seen.add(num)
+
+	return False
+
+
+print(contains([1,2,3,1]))
+print(contains([1,2,3,4]))
