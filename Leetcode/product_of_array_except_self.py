e15b338cc0fc1a79e18879aaa60bdec6e58b32bc
yahya09206 <yahyahussein91@gmail.com>
2026-06-04 23:45:32 -0700

    logic for product of array is complete

--- Leetcode/product_of_array_except_self.py
+++ Leetcode/product_of_array_except_self.py
@@ -1,3 +1,20 @@
 def product_of_array_except_self(nums: List[int]) -> List[int]:
 
+	prefix = [1] * len(nums)
+	suffix = [1] * len(nums)
+
+	for i in range(1, len(nums)):
+		prefix[i] = nums[i - 1] * prefix[i - 1]
 	
+
+	for j in range(len(nums) - 2, -1, -1):
+		suffix[j] = nums[j + 1] * suffix[j + 1]
+
+
+	result = []
+
+	for i in range(0, len(nums)):
+		result.append(prefix[i] * suffix[i])
+
+	return result
+
