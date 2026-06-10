d0c26f8dbce2bdf49e447771d7da726aba685c12
yahya09206 <yahyahussein91@gmail.com>
2026-06-09 22:37:34 -0700

    will be moving this to the sliding window section

--- Leetcode/find_all_anagrams_in_string.py
+++ Leetcode/find_all_anagrams_in_string.py
@@ -2,3 +2,39 @@ def find_all_anagrams(s:str, p:str) -> List[int]:
 
 	if len(p) > len(s):
 		return []
+
+
+	p_count = {}
+	s_count = {}
+
+	for char in p:
+		p_count[char] = p_count.get(char, 0) + 1
+
+
+	result = []
+	left = 0
+
+	for right in range(len(s)):
+
+		incoming_char = s[right]
+
+
+		s_count[incoming_char] = s_count.get(incoming_char, 0) + 1
+
+		if (right - left + 1) > len(p):
+
+			outgoing_char = s[left]
+
+
+			s_count[outgoing_char] -= 1
+
+			if s_count[outgoing_char] == 0:
+				del s_count[outgoing_char]
+
+			left += 1
+
+		if s_count == p_count:
+			result.append(left)
+
+
+	return result
