e6b8dfa4b3dc1b2007befe249b7d9ec7a79705ed
yahya09206 <yahyahussein91@gmail.com>
2026-06-11 22:19:55 -0700

    logic for longest consecutive sequence is complete

--- Leetcode/longest_consecutive_sequence.py
+++ Leetcode/longest_consecutive_sequence.py
@@ -1,1 +1,26 @@
 def longest_consecutive(nums:List[int]) -> int:
+
+	num_set = set(nums)
+	longest_streak = 0
+
+
+	for num in num_set:
+
+		if num - 1 not in num_set:
+
+			current_num = num
+			current_streak = 1
+
+			while current_num + 1 in num_set:
+
+				current_num += 1
+				current_streak += 1
+			
+			longest_streak = max(longest_streak, current_streak)
+
+	return longest_streak
+
+
+print(longest_consecutive([100,4,200,1,3,2]))
+print(longest_consecutive([0,3,7,2,5,8,4,6,0,1]))
+print(longest_consecutive([1,0,1,2]))
