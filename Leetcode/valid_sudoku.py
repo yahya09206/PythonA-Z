61302bec68be3e8b1970da533fa727bdf334a21c
yahya09206 <yahyahussein91@gmail.com>
2026-06-14 23:38:22 -0700

    logic for valid sudoku is completed

--- Leetcode/valid_sudoku.py
+++ Leetcode/valid_sudoku.py
@@ -3,7 +3,7 @@ def valid_sudoku(board:List[List[str]]) -> bool:
 	# need to declare dicts
 	rows = collections.defaultdict(set)
 	cols = collections.defaultdict(set)
-	cols = collections.defaultdict(set)
+	boxes = collections.defaultdict(set)
 
 	for r in range(9):
 		for c in range(9):
@@ -15,3 +15,12 @@ def valid_sudoku(board:List[List[str]]) -> bool:
 
 			# calculate the box tuple key using r // 3 and c // 3
 			box_key = (r // 3, c // 3)
+
+			if val in rows[r] or val in cols[c] or val in boxes[box_key]:
+				return False
+			else:
+				rows[r].add(val)
+				cols[c].add(val)
+				boxes[box_key].add(val)
+
+	return True
