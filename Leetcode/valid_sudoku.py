029cdbe3f83cc7ae4e0d7dd8ef1f9fe3c4a505a9
yahya09206 <yahyahussein91@gmail.com>
2026-06-14 23:31:07 -0700

    looping through board and skipping empty boxes

--- Leetcode/valid_sudoku.py
+++ Leetcode/valid_sudoku.py
@@ -5,4 +5,10 @@ def valid_sudoku(board:List[List[str]]) -> bool:
 	cols = collections.defaultdict(set)
 	cols = collections.defaultdict(set)
 
-	
+	for r in range(9):
+		for c in range(9):
+			val = board[r][c]
+
+			# skip if the value is an empty square
+			if val == ".":
+				continue
