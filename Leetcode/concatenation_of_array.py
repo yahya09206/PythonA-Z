e6815bfa24f665c1d95569f202de7cc02717571b
yahya09206 <yahyahussein91@gmail.com>
2026-06-17 22:52:54 -0700

    logic for concatenation of two arrays is complete

--- Leetcode/concatenation_of_array.py
+++ Leetcode/concatenation_of_array.py
@@ -1,1 +1,12 @@
 def concatenation_of_array(nums:List[int]) -> List[int]:
+
+	n = len(nums)
+	ans = [0] * (2 * n)
+
+	for i in range(n):
+
+		ans[i] = nums[i]
+		ans[i + n] = nums[i]
+
+	return ans
+
