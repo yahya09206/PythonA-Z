81354837dc0ac16df21feb262eb572858ffae86d
yahya09206 <yahyahussein91@gmail.com>
2026-06-30 23:47:13 -0700

    first attempt at 3sum logic

--- Leetcode/three_sum.py
+++ Leetcode/three_sum.py
@@ -1,1 +1,28 @@
 def three_sum(nums:List[int]) -> List[List[int]]:
+
+	result = []
+
+	nums.sort()
+
+	for i, a in enumerate(nums):
+
+		if i > 0 and a == nums[i - 1]:
+			continue
+
+
+		l = i + 1
+		r = len(nums) - 1
+
+		while l < r:
+
+			three_sum = a + nums[l] + nums[r]
+
+			if three_sum < 0:
+				l += 1
+			elif three_sum > 0:
+				r -= 1
+			else:
+				result.append(a, nums[l], nums[r])
+
+				l += 1
+				r -= 1
