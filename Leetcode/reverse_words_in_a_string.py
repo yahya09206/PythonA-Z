3b5acbcf88b94edad33bddc51ee8e1e5f125e253
yahya09206 <yahyahussein91@gmail.com>
2026-07-08 23:13:21 -0700

    logic for reverse words in a string is complete

--- Leetcode/reverse_words_in_a_string.py
+++ Leetcode/reverse_words_in_a_string.py
@@ -1,1 +1,25 @@
 def reverse_words_in_a_string(s:str) -> str:
+
+	l = len(s) - 1
+	r = len(s) - 1
+	words = []
+
+	while r >= 0:
+
+		while r >= 0 and s[r] == ' ':
+			r -= 1
+
+		l = r
+		while l >= 0 and s[l] != ' ':
+			l -= 1
+
+		words.append(s[l + 1: r + 1])
+
+		r = l
+
+	return ' '.join(words)
+
+
+print(reverse_words_in_a_string("the sky is blue"))
+print(reverse_words_in_a_string("  hello world  "))
+print(reverse_words_in_a_string("a good   example"))
