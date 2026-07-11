b2e1877c620a5d3238e1eefd61836b1c028f225c
yahya09206 <yahyahussein91@gmail.com>
2026-07-10 23:25:08 -0700

    completed brute force of squares of sorted array

---
+++ Leetcode/squares_of_sorted_array.py
@@ -0,0 +1,17 @@
+def squares_of_sorted_array(nums:List[int]) -> List[int]:
+
+	# res = []
+
+	# for num in nums:
+
+	# 	res.append(num**2)
+
+	# res.sort()
+	
+	# return res
+	
+
+
+print(squares_of_sorted_array([-4,-1,0,3,10]))
+
+
