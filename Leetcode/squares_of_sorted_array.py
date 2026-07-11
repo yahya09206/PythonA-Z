4b4238c9f441ebf5b9dbcf659355913964c39b07
yahya09206 <yahyahussein91@gmail.com>
2026-07-10 23:45:49 -0700

    optimized squares of sorted arrya using 2 pointer method

--- Leetcode/squares_of_sorted_array.py
+++ Leetcode/squares_of_sorted_array.py
@@ -1,5 +1,27 @@
 def squares_of_sorted_array(nums:List[int]) -> List[int]:
 
+	l = 0
+	r = len(nums) - 1
+	res = [0] * len(nums)
+	p = len(res) - 1
+
+
+	while l <= r:
+
+		left_square = nums[l] * nums[l]
+		right_square = nums[r] * nums[r]
+
+		if left_square < right_square:
+			res[p] = right_square
+			r -= 1
+		else:
+			res[p] = left_square
+			l += 1
+			
+		p -= 1
+
+	return res
+
 	# res = []
 
 	# for num in nums:
