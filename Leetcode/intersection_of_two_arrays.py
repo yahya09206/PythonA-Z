79a8d6468c1f0169b26bbd855ac9a991e74d16a2
yahya09206 <yahyahussein91@gmail.com>
2026-07-11 22:52:04 -0700

    completed brute force for intersection

--- Leetcode/intersection_of_two_arrays.py
+++ Leetcode/intersection_of_two_arrays.py
@@ -1,3 +1,14 @@
 def intersection_of_two_arrays(nums1:List[int], nums2:List[int]) -> List[int]:
 
-	
+	res = list()
+
+	for i in nums1:
+		for j in nums2:
+			if i == j and i not in res:
+				res.append(i)
+
+	return res
+
+
+print(intersection_of_two_arrays([1,2,2,1], [2,2]))
+print(intersection_of_two_arrays([4,9,5], [9,4,9,8,4]))
