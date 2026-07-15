60ecf870f7eb1578a6b59db4b9dbf157f930e265
yahya09206 <yahyahussein91@gmail.com>
2026-07-14 23:13:00 -0700

    completed brute force for removing dupes from sorted array

--- Leetcode/remove_duplicates_from_sorted.py
+++ Leetcode/remove_duplicates_from_sorted.py
@@ -1,3 +1,13 @@
 def remove_duplicates_from_sorted(nums:List[int]) -> int:
 
-	
+	result = []
+
+	for i in nums:
+		if i not in result:
+			result.append(i)
+
+	return result
+
+
+print(remove_duplicates_from_sorted([1,1,2]))
+print(remove_duplicates_from_sorted([0,0,1,1,1,2,2,3,3,4]))
